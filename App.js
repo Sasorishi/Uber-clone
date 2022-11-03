36c786bd880728622572ee96938c36398290bb1c
Sasorishi <on_alban@yahoo.fr>
2022-11-03 19:47:27 +0100

    Implementing Keyboard Avoinding View

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, KeyboardAvoidingView, Platform } from 'react-native';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
@@ -16,23 +16,28 @@ export default function App() {
     <Provider store={store}>
       <NavigationContainer>
         <SafeAreaProvider>
-          <Stack.Navigator>
-            <Stack.Screen 
-              name='HomeScreen'
-              component={HomeScreen}
-              options={{
-                headerShown: false
-              }}
-            />
-            <Stack.Screen 
-              name='MapScreen'
-              component={MapScreen}
-              options={{
-                headerShown: false
-              }}
-            />
-          </Stack.Navigator>
-          {/* <HomeScreen /> */}
+          <KeyboardAvoidingView
+            behavior={Platform.OS === 'ios' ? "padding": 'height'}
+            style={{ flex: 1 }}
+            keyboardVerticalOffset={Platform.OS === 'ios' ? -64: 0}
+            >
+            <Stack.Navigator>
+              <Stack.Screen 
+                name='HomeScreen'
+                component={HomeScreen}
+                options={{
+                  headerShown: false
+                }}
+              />
+              <Stack.Screen 
+                name='MapScreen'
+                component={MapScreen}
+                options={{
+                  headerShown: false
+                }}
+              />
+            </Stack.Navigator>
+          </KeyboardAvoidingView>
         </SafeAreaProvider>
       </NavigationContainer>
     </Provider>
