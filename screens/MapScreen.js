20c6bb59ae4f5b7b36aad975958fcb0ceaae7af0
Sasorishi <on_alban@yahoo.fr>
2022-11-03 08:24:53 +0100

    Building the MapScreen Component

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -2,11 +2,16 @@ import React from "react";
 import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
 import tw from 'twrnc'
 import NavOptions from "../components/NavOptions";
+import MapView from "react-native-maps";
+import Map from "../components/Map";
 
 const MapScreen = () => {
     return (
         <View>
-            
+            <View style={tw `h-1/2`}>
+                <Map />
+            </View>
+            <View style={tw `h-1/2`}></View>
         </View>
     )
 }
