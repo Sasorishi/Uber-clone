70446fd270eda3f45a6d88e0fc210fce0bcfcc26
Sasorishi <on_alban@yahoo.fr>
2022-11-03 09:20:58 +0100

    Implementing Directions API

--- components/Map.js
+++ components/Map.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import { useSelector } from "react-redux";
-import tw from 'twrnc'
-import { selectOrigin } from "../slices/navSlice";
+import tw from 'twrnc';
+import { selectDestination, selectOrigin } from "../slices/navSlice";
+import MapViewDirections from "react-native-maps-directions";
+import { GOOGLE_MAPS_APIKEY } from '@env';
+import { useRef } from "react";
 
 const Map = () => {
     const origin = useSelector(selectOrigin)
+    const destination = useSelector(selectDestination)
+    const mapRef = useRef(null)
+
+    useEffect(() => {
+        if (!origin || !destination) return; //Break this code if condition not valid
+        mapRef.current.fitToSuppliedMarkers(['origin', 'destination'], {edgePadding: {top: 50, right: 50, bottom: 50, left: 50}})
+    }, [origin, destination])
+
     return (
         <MapView
             style={tw `flex-1`}
@@ -19,6 +30,15 @@ const Map = () => {
                 longitudeDelta: 0.0421
             }}
         >
+            {origin && destination && (
+                <MapViewDirections
+                    origin={origin.description}
+                    destination={destination.description}
+                    apikey={GOOGLE_MAPS_APIKEY}
+                    strokeWidth={3}
+                    strokeColor="black"
+                />
+            )}
             {origin?.location && (
                 <Marker
                     coordinate = {{
