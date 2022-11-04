e5fff20f4aa2f87113ed227d838d2f0c528009bb
Sasorishi <on_alban@yahoo.fr>
2022-11-04 06:10:57 +0100

    Implementing the Travel Time Calculation

--- components/Map.js
+++ components/Map.js
@@ -1,9 +1,9 @@
 import React, { useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import MapView, { Marker } from "react-native-maps";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import tw from 'twrnc';
-import { selectDestination, selectOrigin } from "../slices/navSlice";
+import { selectDestination, selectOrigin, setTravelTimeInformation } from "../slices/navSlice";
 import MapViewDirections from "react-native-maps-directions";
 import { GOOGLE_MAPS_APIKEY } from '@env';
 import { useRef } from "react";
@@ -12,11 +12,25 @@ const Map = () => {
     const origin = useSelector(selectOrigin)
     const destination = useSelector(selectDestination)
     const mapRef = useRef(null)
+    const dispatch = useDispatch()
 
     useEffect(() => {
         if (!origin || !destination) return; //Break this code if condition not valid
         mapRef.current.fitToSuppliedMarkers(['origin', 'destination'], {edgePadding: {top: 50, right: 50, bottom: 50, left: 50}})
     }, [origin, destination])
+
+    useEffect(() => {
+        if (!origin || !destination) return;
+        const getTravelTime = async () => {
+            fetch(`https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${origin.description}&destination=${destination.description}&key=${GOOGLE_MAP_APIKEY}`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                dispatch(setTravelTimeInformation(data.rows[0].elements[0]))
+            })
+        }
+        getTravelTime();
+    }, [origin, destination, GOOGLE_MAPS_APIKEY])
 
     return (
         <MapView
--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -3,6 +3,8 @@ import React, { useState } from 'react'
 import tw from 'twrnc';
 import { Icon } from '@rneui/base';
 import { useNavigation } from '@react-navigation/native';
+import { useSelector } from "react-redux";
+import { selectTravelTimeInformation } from "../slices/navSlice";
 
 const data = [
   {
@@ -28,6 +30,7 @@ const data = [
 const RideOptionsCard = () => {
   const navigation = useNavigation();
   const [selected, setSelected] = useState(null);
+  const travelTimeInformation = useSelector(selectTravelTimeInformation);
   return (
     <SafeAreaView style={tw `bg-white flex-grow`}>
       <View>
@@ -37,7 +40,7 @@ const RideOptionsCard = () => {
           >
           <Icon name="chevron-left" type="fontawesome" />
         </TouchableOpacity>
-        <Text style={tw `text-center py-5 text-xl`}>Select a ride</Text>
+        <Text style={tw `text-center py-5 text-xl`}>Select a ride - {travelTimeInformation?.distance.text}</Text>
       </View>
       <FlatList 
         data={data}
@@ -57,7 +60,7 @@ const RideOptionsCard = () => {
             />
             <View style={tw`-ml-6`}>
               <Text style={tw`text-xl font-semibold`}>{ title }</Text>
-              <Text>Travel time ...</Text>
+              <Text>{travelTimeInformation?.duration.text}</Text>
             </View>
             <Text style={tw`text-xl`}>$ 99</Text>
           </TouchableOpacity>
